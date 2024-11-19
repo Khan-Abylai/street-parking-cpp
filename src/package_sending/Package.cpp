064f319d1bca7731f2315b1f33be70be478a71db
arman <a.yeleusinov@parqour.com>
2024-11-19 12:23:18 +0500

    changed package format

--- src/package_sending/Package.cpp
+++ src/package_sending/Package.cpp
@@ -30,6 +30,43 @@ std::string Package::convertVectorToRawString(std::vector<std::string> lps) {
     return oss.str();
 }
 
+std::vector<std::map<std::string, std::string>> convertToDictList(
+    const std::vector<std::string>& plateNumbers, const std::vector<std::string>& licensePlateBBoxes){
+    std::vector<std::map<std::string, std::string>> result;
+
+    size_t maxSize = plateNumbers.size();
+
+    for (size_t i = 0; i < maxSize; ++i)
+    {
+        std::map<std::string, std::string> entry;
+        entry["car_number"] = plateNumbers[i];
+        entry["lp_rect"] = licensePlateBBoxes[i];
+
+        result.push_back(entry);
+    }
+
+    return result;
+}
+
+std::string convertDictToRawString(const std::vector<std::map<std::string, std::string>>& cars) {
+    std::ostringstream oss;
+    json packageJson;
+    oss << "[";
+    for (size_t i = 0; i < cars.size(); ++i) {
+        const auto& car = cars[i];
+        oss << "{";
+        oss << "\"car_number\": \"" << car.at("car_number") << "\" ";
+        // oss << ", ";
+        // oss << "\"lp_rect\": \"" << car.at("lp_rect") << "\" ";
+        oss << "}";
+        if (i < cars.size() - 1) {
+            oss << ", ";
+        }
+    }
+    oss << "]";
+    return oss.str();
+}
+
 
 const std::string &Package::getCameraIp() const {
     return cameraIp;
@@ -37,11 +74,13 @@ const std::string &Package::getCameraIp() const {
 
 std::string Package::getPackageJson() const {
     json packageJson;
-    packageJson["camera_ip"] = cameraIp;
-    packageJson["license_plate_labels"] = convertVectorToRawString(licensePlateLabels);
-    packageJson["image"] =  Utils::encodeImgToBase64(carImage);
+    packageJson["ip_address"] = cameraIp;
+    packageJson["cars"] =  convertDictToRawString(convertToDictList(licensePlateLabels, licensePlateBBoxes));
     packageJson["event_time"] = Utils::dateTimeToStr(eventTime);
-    packageJson["license_plate_bboxes"] = convertVectorToRawString(licensePlateBBoxes);
+    packageJson["picture"] =  Utils::encodeImgToBase64(carImage);
+    // packageJson["license_plate_labels"] = convertVectorToRawString(licensePlateLabels);
+    // packageJson["license_plate_bboxes"] = convertVectorToRawString(licensePlateBBoxes);
+
     return packageJson.dump();
 }
 
